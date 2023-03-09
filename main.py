527a056f132c027fd5c7399d1ec90c4192d2e165
CooperE314 <c.engebretson@ufl.edu>
2023-03-09 12:51:27 -0500

    encoder

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 def print_menu():
-    print('Menu \n-------------\n 1. Encode\n2. Decode\n3. Quit')
+    print('Menu \n-------------\n1. Encode\n2. Decode\n3. Quit')
 
-def encode(password_input):
-    pass
-def decode():
-    pass
-
-"""def main():
-    #print menu
+def encode(password_input): #123
+    encoded_pass = ''
+    for i in password_input:
+        encoded_pass += str(int(i) + 3)
+    return encoded_pass
 
 
+'''def decode(password_input):
+    decoded_pass = ''
+    for i in password_input:
+        decoded_pass += str(int(i) - 3)
+    return decoded_pass'''
 
-if __name__ == "__main__"
-    main()"""
 
-password_input = '12345555'
-encoded_pass = ''
-for i in password_input:
-    encoded_pass += str(int(i) + 3)
+def main():
+    keep_going = True
+    while keep_going == True:
+        print_menu()
+        _option = int(input('Please enter an option:'))
+        print('')
+        if _option == 1:
+            password_input = input('Please enter your password to encode:')
+            encoded_pass = encode(password_input)
+            print('Your password has been encoded and stored!')
+            print('')
 
-print(encoded_pass)
+        elif _option == 2:
+            decoded_pass = decode(encoded_pass)
+            print('The encoded password is',encoded_pass,', and the original password is',decoded_pass)
+        elif _option ==3:
+            keep_going = False
+            break
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 
